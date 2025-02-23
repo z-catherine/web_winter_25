f02d46afdaed4651fd6aed684a520d42acbf6f57
z-catherine <czhang@eastsideprep.org>
2025-02-22 20:35:54 -0800

    Add files via upload

---
+++ final/js/script.js
@@ -0,0 +1,14 @@
+function pFunThree() {
+    var p = document.getElementById('p-fun-three');
+    p.innerHTML = ' web!';
+}
+
+    function pFunTwo() {
+    var p = document.getElementById('p-fun-two');
+    p.innerHTML = ' math!';
+}
+
+    function pFunOne() {
+    var p = document.getElementById('p-fun-one');
+    p.innerHTML = ' science!';
+}
